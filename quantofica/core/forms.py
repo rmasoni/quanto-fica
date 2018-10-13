b62fda885565df377be56a8b1fa7c69348c17896
Rafael Masoni <rafaelmasoni@gmail.com>
2018-10-13 05:29:17 -0300

    Transport input from forms.py to the template
    Also remove label, add disclaimer to output.

--- quantofica/core/forms.py
+++ quantofica/core/forms.py
@@ -6,28 +6,23 @@ from .models import Currency
 
 
 class NubankExchangeRateForm(forms.Form):
+
     currency = forms.ModelChoiceField(
-        queryset=Currency.objects.all(),
-        empty_label=None,
-        required=True,
-        widget=forms.Select(
-            attrs={
+        queryset = Currency.objects.all(),
+        empty_label = None,
+        required = True,
+        widget = forms.Select(
+            attrs = {
+                'id': 'input__unit',
                 'class': 'input__unit'
             },
         )
     )
+
     value = forms.DecimalField(
-        max_digits=10,
-        decimal_places=2,
-        label='Valor da compra',
-        required=True,
-        widget=forms.NumberInput(
-            attrs={
-                'autofocus': 'autofocus',
-                'class': 'input__field',
-                'step': 'any'
-            },
-        )
+        max_digits = 10,
+        decimal_places = 2,
+        required = True
     )
 
     def calculate(self):
